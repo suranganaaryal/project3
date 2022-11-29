247ee475cbd1714394cee4d6479914584c10e6cd
suranganaaryal <surangana.aryal@bison.howard.edu>
2022-11-28 22:29:59 -0500

    processing the input string

--- project3.s
+++ project3.s
@@ -11,3 +11,17 @@ li $v0,8
 la $a0,input 					
 li $a1, 1001 								
 syscall
+
+li $t5,0
+
+jal sub_a
+
+forward:
+j display
+
+sub_a:
+sub $sp, $sp,4
+sw $a0, 0($sp)
+lw $t0, 0($sp)
+addi $sp,$sp,4
+move $t6, $t0
